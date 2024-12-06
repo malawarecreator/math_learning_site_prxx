ca664455350758979e98c64cf6877f94aea1bc60
malawarecreator <165108205+malawarecreator@users.noreply.github.com>
2024-12-06 20:32:18 +0000

    deleted:    package-lock.json 	modified:   source/aboutme.html 	modified:   source/home.html 	modified:   source/index.js

--- source/index.js
+++ source/index.js
@@ -6,8 +6,12 @@ const app = express();
 
 app.get("/", (req, res) => {
     //send html file
-    res.sendFile(path.join(__dirname, "/aboutme.html"))
+    res.sendFile(path.join(__dirname, "/home.html"))
+    
 });
+app.get("/aboutme", (req, res) => {
+    res.sendFile(path.join(__dirname, "/aboutme.html"))
+})
 
 app.listen(process.env.PORT || 8080, () => {
     console.log("server started")
