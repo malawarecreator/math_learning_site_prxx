5c651c52e42d0378bd70dccba8bb68d004ecd643
malawarecreator <165108205+malawarecreator@users.noreply.github.com>
2024-12-09 18:26:17 +0000

    modified:   source/index.js 	new file:   source/middleware.js

--- source/index.js
+++ source/index.js
@@ -1,55 +1,19 @@
 
 const express = require("express")
 const path = require("path")
-
+const auth_middleware = require("./middleware")
 const app = express();
-
-app.get("/", (req, res, next) => {
-
-
-    switch (req.query.id) {
-        case "ben_dev":
-            console.log("ben authed")
-            next()
-            break
-        case "sameek_dev":
-            console.log("tim authed")
-            next()
-            break
-        case undefined:
-            res.sendFile(path.join(__dirname, "/access_denied.html"))
-            console.log("user denied access")
-            break
-
-    }
-    
-}, (req, res) => {
+app.use(auth_middleware)
+app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "/home.html"))
 }
 );
-app.get("/aboutme",(req, res, next) => {
-    switch (req.query.id) {
-        case "ben_dev":
-            console.log("ben authed")
-            next()
-            break
-        case "sameek_dev":
-            console.log("tim authed")
-            next()
-            break
-        default:
-            res.sendFile(path.join(__dirname, "/access_denied.html"))
-            console.log("user denied access")
-            break
-
-    }
-    
-
-} ,(req, res) => {
+app.get("/aboutme", (req, res) => {
     res.sendFile(path.join(__dirname, "/aboutme.html"))
 })
 
 app.listen(process.env.PORT || 8080, () => {
     console.log("server started")
 });
+
 
