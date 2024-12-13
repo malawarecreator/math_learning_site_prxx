e711f7b6d7498bdce5bc58994ae2c626bb5c0d01
IntelN3on <intellectualn3on@outlook.com>
2024-12-13 02:03:23 +0000

    completed pass page (works!)

--- source/middleware.js
+++ source/middleware.js
@@ -1,23 +1,23 @@
 
-const path = require("path")
-const auth_middleware = (req, res, next) => {
-    
-    switch (req.query.id) {
-        case "ben_dev":
-            console.log("ben authed")
-            next()
-            break
-        case "neon_dev":
-            console.log("sameek authed")
-            next()
-            break
-        case undefined:
-            res.sendFile(path.join(__dirname, "/access_denied.html"))
-            console.log("user denied access")
-            break
-
-    }
-}
+  const path = require("path")
+  const auth_middleware = (req, res, next) => {
+      
+      switch (req.query.id) {
+          case "ben_dev":
+              console.log("ben authed")
+              next()
+              break
+          case "neon_dev":
+              console.log("sameek authed")
+              next()
+              break
+       case undefined:
+              res.sendFile(path.join(__dirname, "/access_denied.html"))
+              console.log("user denied access")
+              break
+  
+      }
+  }
 
 
 module.exports = auth_middleware
